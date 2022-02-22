6e6e482c416ff718d3fe8d6cbaae484e5252263d
SebastianStreng <51957686+SebastianStreng@users.noreply.github.com>
2022-02-22 09:54:53 +0100

    add gui

---
+++ WpfTestApp/DataHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestApp
+{
+    internal class DataHandler
+    {
+        private void Filter(List<string> list1, Dictionary<string, string> dict)
+        {
+            var selection = dict.Select(x => x.Value).Where(x => x.)
+        }
+
+        private Dictionary<string, string> CombineTwoListsToDict(List<string> list1, List<string> list2)
+        {
+            Dictionary<string, string> filters = new Dictionary<string, string>();
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                filters.Add(list1[i], list2[i]);
+            }
+            return filters;
+        }
+    }
+}
---
+++ WpfTestApp/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowComboBox(ComboBox combobox, Label label)
+        {
+            combobox.Visibility = Visibility.Collapsed;
+            label.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowTextBox(TextBox textBox, Label label)
+        {
+            textBox.Visibility = Visibility.Collapsed;
+            label.Visibility = Visibility.Collapsed;
+        }
+
+        private void BlockAccessInTextBoxIfCheckboxIsFalse(CheckBox checkbox, TextBox textbox)
+        {
+            if (checkbox.IsChecked == false)
+            {
+                textbox.IsReadOnly = true;
+            }
+        }
+
+        private void BlockAccessInComboBoxIfCheckboxIsFalse(CheckBox checkbox, ComboBox comboBox)
+        {
+            if (checkbox.IsChecked == false)
+            {
+                comboBox.IsManipulationEnabled = false; //Or "isEnabled"
+            }
+        }
+
+        private bool CheckIfTextBoxIsNotEmpty(TextBox textbox)
+        {
+            if (!string.IsNullOrEmpty(textbox.Text))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
